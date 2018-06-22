b898995522212d474d98f4bc787177f33ebb2a92
phantomhieve <khetanatulz@gmail.com>
2018-06-22 21:13:33 +0530

    time to change pre gray

--- motion.py
+++ motion.py
@@ -7,6 +7,8 @@ vs = VideoStream(src=0).start()
 time.sleep(2.0)
 # initializing the first frame of the video by None
 firstFrame = None
+# start time
+start = time.time()
 # loop over the frames of the video
 while True:
     # readind the current frame
@@ -16,15 +18,16 @@ while True:
         break
     #resize the frame , convert to grayscale and blur it
     #   - Resizing the frame
-    frame = imutils.resize(frame,width=500)
+    frame = imutils.resize(frame,width=700)
     #   - Converting to gray scale
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     #   - Blurring it
     gray = cv2.GaussianBlur(gray,(21,21),0)
     # if the first frame is None initialize it with converted frame
-    if firstFrame is None:
+    # or time difference is greater than 15 sec
+    if firstFrame is None or time.time()-start > 15:
+        start = time.time()
         firstFrame = gray
-        continue
     # compute the absolute difference between current frame and previous frame
     frameDelta = cv2.absdiff(firstFrame,gray)
     # grayscaling the image
@@ -39,7 +42,7 @@ while True:
     #loop over the contours
     for c in cnts:
         # if contour is too small ignore it
-        if cv2.contourArea(c) < 200:
+        if cv2.contourArea(c) < 500:
             continue
         # compute the bounding box for the contour and draw it to the frame
         (x, y, w, h) = cv2.boundingRect(c)
